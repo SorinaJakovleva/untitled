395ca420a0bf0f6f6094095f9f04209bcfa48bb6
SorinaJakovleva <112900403+SorinaJakovleva@users.noreply.github.com>
2022-09-26 21:50:19 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mani sauc Sorina");
+        System.out.println("Es esmu students");
+    }
+}
---
+++ src/main/java/lekcijaViens/labDarbs/main.java
@@ -0,0 +1,85 @@
+package lekcijaViens.labDarbs;
+
+public class main {
+    public static void main(String[] args) {
+        //int - integer , satur veselu skaitli
+        int temperatura = -10;
+        System.out.println("Ziemā būs: " + temperatura);
+
+        int vecums = 42;
+        System.out.println("Mans vecums ir:" + vecums);
+        int garums = 174;
+        System.out.println("Mans garums ir:" + garums);
+        int kurpjuIzmers = 41;
+        System.out.println("Mans kurpju izmērs ir:" + kurpjuIzmers);
+        int svars = 65;
+        System.out.println("Svars:" + svars);
+        int darbaStazs = 19;
+        System.out.println("Darba stāžs:" + darbaStazs);
+
+        //Maksimālā integer vērtība
+        int maxInt = 2147483647;
+        //pieņem veselu skaitli no -127 līdz +127
+        byte vecumsDivi = 99;
+
+        short latvijasMazpilseta = 17000;
+
+        long zivisOkeana = 21474836499L;
+//
+        String sunaVards = "Reksis";
+        String teksts = "sunaVards ir: ";
+        System.out.println(teksts + sunaVards);
+        boolean vaiIrPotets = true;
+
+
+float apavuIzmers = 44.5F;
+System.out.println("apavu izmers " + apavuIzmers);
+
+double grauduSvars = 158.3;
+System.out.println("Tika pārdoti  " + grauduSvars + "kg graudi");
+
+//Šodien ir sestdiena, lielveikals ir atvērts
+        boolean isSuperMarketOpen = true;
+        //Turp
+        boolean isSchoolOpen = false;
+        System.out.println("Vai veikals ir atvērts?" + isSuperMarketOpen);
+
+String sunaSkirne = "VAS";
+char valuta = '$';
+char dzimums = 'S';
+        char atzime = 'A';
+
+System.out.println("test" + valuta + atzime + dzimums);
+int a = 5;
+int b = 10;
+int summa;
+summa = a + b;
+
+System.out.println(summa);
+String name = "Sorina";
+System.out.println(name);
+String name2 = "Juris";
+System.out.println(name);
+
+int x = 10;
+int y = -45;
+int rezultats = x + y;
+System.out.println(rezultats);
+
+rezultats = x-y;
+System.out.println(rezultats);
+
+rezultats = x*y;
+System.out.println(rezultats);
+
+rezultats = x/y;
+System.out.println(rezultats);
+
+rezultats = 10/2;
+System.out.println(rezultats);
+rezultats = 11%2;
+System.out.println(rezultats);
+           rezultats = 5*(3+7) /2;
+           System.out.println(rezultats);
+    }
+}
